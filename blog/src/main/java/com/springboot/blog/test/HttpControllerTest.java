a79ae437f568c0cc4f32ff5e8c783498bf14607e
leejuyeol <ljykimc159@gmail.com>
2021-06-25 15:23:08 +0900

    http 테스트 위한 컨트롤러 생성

--- blog/src/main/java/com/springboot/blog/test/HttpControllerTest.java
+++ blog/src/main/java/com/springboot/blog/test/HttpControllerTest.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 // 사용자가 요청 -> HTML파일
@@ -16,20 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class HttpControllerTest {
 	
 		@GetMapping("/http/get")
-		public String getTest() {
-			return "get 요청";
+		public String getTest(Member m) { // url로 id=1&password=1234 보낼때 MessageConverter(스프링 부트) 가 수행
+			return "get 요청 " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", "  + m.getEmail();
 		}
 		
 		@PostMapping("/http/post")
-		public String postTest() {
-			return "post 요청";
+		public String postTest(@RequestBody Member m) { // @RequestBody Stirng text -> text/plain(텍스트형태로 값 보낼때), 
+																												// @RequestBody Member m -> application/json(json형태로 데이터 보낼때) -> MessageConverter(스프링 부트) 가 수행
+			return "post 요청 " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", "  + m.getEmail();
 		}
 		
 		@PutMapping("/http/put")
-		public String putTest() {
-			return "put 요청";
+		public String putTest(@RequestBody Member m) {  
+			return "put 요청 " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", "  + m.getEmail();
 		}
-		
+		 
 		@DeleteMapping("/http/delete")
 		public String deleteTest() {
 			return "delete 요청";
