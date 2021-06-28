2443554255202b470949069227851cdfb82e8637
leejuyeol <ljykimc159@gmail.com>
2021-06-28 09:04:16 +0900

    lombok builder 테스트

--- blog/src/main/java/com/springboot/blog/test/HttpControllerTest.java
+++ blog/src/main/java/com/springboot/blog/test/HttpControllerTest.java
@@ -15,12 +15,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HttpControllerTest {
-	
-		@GetMapping("/http/get")
+		private static final String TAG = "HttpControllerTest";
+		
+		@GetMapping("/lombok/test")
+		public String lombokTest() {
+			// builder를 통해 생성자에 넣고싶은 인자만 넣을 수 있고 순서 상관없이 사용 가능(lombok)
+			Member m = Member.builder().email("cos@naver.com").password("1234").username("cos").build();
+			System.out.println(TAG + " getter : " + m.getId());
+			m.setId(5000); 
+			System.out.println(TAG + " setter : " + m.getId()); 
+			
+			return "lombok test"; 
+		}
+		
+		@GetMapping("/http/get")  
 		public String getTest(Member m) { // url로 id=1&password=1234 보낼때 MessageConverter(스프링 부트) 가 수행
 			return "get 요청 " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", "  + m.getEmail();
 		}
-		
+		 
 		@PostMapping("/http/post")
 		public String postTest(@RequestBody Member m) { // @RequestBody Stirng text -> text/plain(텍스트형태로 값 보낼때), 
 																												// @RequestBody Member m -> application/json(json형태로 데이터 보낼때) -> MessageConverter(스프링 부트) 가 수행
@@ -31,7 +43,7 @@ public class HttpControllerTest {
 		public String putTest(@RequestBody Member m) {  
 			return "put 요청 " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", "  + m.getEmail();
 		}
-		 
+		  
 		@DeleteMapping("/http/delete")
 		public String deleteTest() {
 			return "delete 요청";
--- blog/src/main/java/com/springboot/blog/test/Member.java
+++ blog/src/main/java/com/springboot/blog/test/Member.java
@@ -1,19 +1,27 @@
 package com.springboot.blog.test;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 //@Getter
 //@Setter
 //@RequiredArgsConstructor // final 붙은 변수 생성자 생성
-@Data // @Getter + @Setter 합친거
-@AllArgsConstructor // 전체생성자
+//@AllArgsConstructor // 전체생성자
 @NoArgsConstructor // 빈 생성자
+@Data // @Getter + @Setter 합친거
 public class Member {
 	private int id;
 	private String username;
 	private String password;
 	private String email;
-	 
+	
+	@Builder
+	public Member(int id, String username, String password, String email) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+	}
 }
