132b22db9a1461fcfdead5e5244791f742d7123b
leejuyeol <ljykimc159@gmail.com>
2021-06-28 16:08:29 +0900

    enum 생성
    user Entity에 role 컬럼 enum 생성

---
+++ blog/src/main/java/com/springboot/blog/model/RoleType.java
@@ -0,0 +1,6 @@
+package com.springboot.blog.model;
+
+// enum은 데이터의 도메인을 만들 때 쓴다. 도메인이라는 것은 어떤 범위를 말함, 즉 어떤 범위안에 데이터를 넣을 때 ex) 남, 여 만 넣으려고 할 때 남자, 여자 이렇게 잘못 넣을 수 있으니 
+public enum RoleType { 
+	USER, ADMIN
+}
