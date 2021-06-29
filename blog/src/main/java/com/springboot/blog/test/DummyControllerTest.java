3a735299555fc8b453e060b7d9fd39d858f82557
leejuyeol <ljykimc159@gmail.com>
2021-06-29 10:12:11 +0900

    유저 select test

--- blog/src/main/java/com/springboot/blog/test/DummyControllerTest.java
+++ blog/src/main/java/com/springboot/blog/test/DummyControllerTest.java
@@ -1,6 +1,10 @@
 package com.springboot.blog.test;
 
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +17,31 @@ public class DummyControllerTest {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	//{id} 주소로 파라미터를 전달 받을 수 있음.
+	// http://localhost:8000/blog/dummy/user/5
+	@GetMapping("/dummy/user/{id}")
+	public User detail(@PathVariable int id) {
+		// user/4을 찾으면 내가 데이터베이스에서 못찾아오게 되면 user가 null이 되니
+		// return null이 리턴 되니, 프로그램에 문제가 됨
+		// Optional로 너의 User객체를 감싸서 가져올테니 null인지 판단해서 return (findById의 리턴값이 Optional
+		// userRepository.findById(id).get()은 null이 없다고 판단하는 함수 (조금위험)
+		//userRepository.findById(id).orElseGet(new Supplier<User>() {@Overridepublic User get() { // 값이 없을 경우 빈 user 반환return new User();}	});
+		User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+			@Override
+			public IllegalArgumentException get() {
+				// TODO Auto-generated method stub
+				return new IllegalArgumentException("해당 유저는 없습니다. + id : " + id);
+			}
+		});
+		
+		// user 객체 = 자바 오브젝트
+		// 변환(웹브라우저가 이해할수 있는 데이터) -> json(Gson 라이브러리)
+		// 스프링부트 = MessageConverter라는 애가 응답시에 자동 작동
+		// 만약에 자바 오브젝트를 리턴하게 되면 MessageConverter가 Jackson 라이브러리를 호출해서
+		// user 오브젝트를 json으로 변환해서 브라우저에게 던져줍니다.
+		return user;
+	} 
 	
 	@PostMapping("/dummy/test")  
 	public String dummyTest(User user) {
