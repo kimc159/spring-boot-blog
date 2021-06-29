67beda0b4db46ac799567b43a6b1b4903455acaa
leejuyeol <ljykimc159@gmail.com>
2021-06-29 10:34:35 +0900

    paging test

--- blog/src/main/java/com/springboot/blog/test/DummyControllerTest.java
+++ blog/src/main/java/com/springboot/blog/test/DummyControllerTest.java
@@ -1,8 +1,13 @@
 package com.springboot.blog.test;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +22,27 @@ public class DummyControllerTest {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@GetMapping("/dummy/users")
+	public List<User> userAll() {
+		
+		return userRepository.findAll();
+	}
+	
+	// 한 페이지당 2건에 데이터를 리턴받아 볼 예정
+	@GetMapping("/dummy/user")
+	public List<User> pageList(@PageableDefault(size=2, sort="id", direction = Sort.Direction.DESC) Pageable pageable) {
+		Page<User> pagingUser = userRepository.findAll(pageable);
+		System.out.println(pagingUser.isFirst());
+		System.out.println(pagingUser.isLast());
+		System.out.println(pagingUser.getSize()); 
+//		if(pagingUser.isFirst()) {
+//			
+//		}
+		
+		List<User> users = pagingUser.getContent();
+		return users;  
+	}
 	
 	//{id} 주소로 파라미터를 전달 받을 수 있음.
 	// http://localhost:8000/blog/dummy/user/5
