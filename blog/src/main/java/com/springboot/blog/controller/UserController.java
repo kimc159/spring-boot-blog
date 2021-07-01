293d2fb3d4e2414cac0b9ddc151ebbe9ecaa448f
leejuyeol <ljykimc159@gmail.com>
2021-07-01 14:18:51 +0900

    join 데이터 저장

--- blog/src/main/java/com/springboot/blog/controller/UserController.java
+++ blog/src/main/java/com/springboot/blog/controller/UserController.java
@@ -1,10 +1,23 @@
 package com.springboot.blog.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.springboot.blog.model.User;
+import com.springboot.blog.model.UserRepository;
 
 @Controller
 public class UserController {
+	
+	@Autowired
+	private UserRepository userRepository;
 	
 	@GetMapping("/user/joinForm")  
 	public String joinForm() { 
@@ -15,6 +28,23 @@ public class UserController {
 	@GetMapping("/user/loginForm")  
 	public String loginForm() { 
 		 
-		return "user/loginForm";   
+		return "user/loginForm";    
 	}
+	
+	@ResponseBody
+	@PostMapping("/user/join")
+	public Map<String, Integer> join(@RequestBody User user) { 
+		int result = 0;
+		
+		User saveUser = userRepository.save(user); 
+		
+		if(saveUser != null) {
+			
+		}
+		
+		Map<String, Integer> map = new HashMap<String, Integer>();	
+		map.put("result", result); 
+		
+		return  map;  
+	} 
 }
