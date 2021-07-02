4116892f80ec6f2015d5fe7b2aa943922a63c3d9
leejuyeol <ljykimc159@gmail.com>
2021-07-02 10:47:38 +0900

    회원가입 디지 저장

--- blog/src/main/java/com/springboot/blog/controller/api/UserApiController.java
+++ blog/src/main/java/com/springboot/blog/controller/api/UserApiController.java
@@ -1,20 +1,31 @@
-package com.springboot.blog.controller;
+package com.springboot.blog.controller.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.blog.dto.ResponseDto;
+import com.springboot.blog.model.RoleType;
 import com.springboot.blog.model.User;
+import com.springboot.blog.service.UserService;
 
 @RestController
 public class UserApiController {
 	
+	@Autowired
+	private UserService userService;
+	
 	@PostMapping("/api/user")
 	public ResponseDto<Integer> save(@RequestBody User user) {
 		
-		// 실제 DB에서 insert하고 아래에서 return
-		return new ResponseDto<Integer>(HttpStatus.OK, 1); 
+		user.setRole(RoleType.USER);
+		int result = userService.회원가입(user); 
+		
+		// 실제 DB에서 insert하고 아래에서 return 
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), result); 
 	}
+	
+	
 }
