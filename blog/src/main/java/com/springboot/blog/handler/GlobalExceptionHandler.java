84c641cb6ce264d3a7d7edb5b125ed546e3e9b7c
leejuyeol <ljykimc159@gmail.com>
2021-07-02 10:46:57 +0900

    Exception Handler return 값 수정

--- blog/src/main/java/com/springboot/blog/handler/GlobalExceptionHandler.java
+++ blog/src/main/java/com/springboot/blog/handler/GlobalExceptionHandler.java
@@ -1,15 +1,18 @@
 package com.springboot.blog.handler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.blog.dto.ResponseDto;
 
 @ControllerAdvice // Exception이 발생했을 때 여길로 들어옴
 @RestController
 public class GlobalExceptionHandler {
 	
-	@ExceptionHandler(value=Exception.class) // IllegalArgumentException이 발생하면 이 함수를 실행하고 e 안에 해당 내용이 들어감
-	public String handleArgumentException(Exception e) {
-		return "<h1>" + e.getMessage() + "</h1>"; 
+	@ExceptionHandler(value=Exception.class) // Exception이 발생하면 이 함수를 실행하고 e 안에 해당 내용이 들어감
+	public ResponseDto<String> handleArgumentException(Exception e) { 
+		return new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()); 
 	}
 }
