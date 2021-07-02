abca57668882b21433866305366ae532c26184c9
leejuyeol <ljykimc159@gmail.com>
2021-07-02 15:46:23 +0900

    Controller login 함수 추가

--- blog/src/main/java/com/springboot/blog/controller/api/UserApiController.java
+++ blog/src/main/java/com/springboot/blog/controller/api/UserApiController.java
@@ -1,5 +1,7 @@
 package com.springboot.blog.controller.api;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +19,9 @@ public class UserApiController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private HttpSession session;
+	
 	@PostMapping("/api/user")
 	public ResponseDto<Integer> save(@RequestBody User user) {
 		
@@ -27,5 +32,15 @@ public class UserApiController {
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), result); 
 	}
 	
-	
+	@PostMapping("/api/user/login")
+	public ResponseDto<Integer> login(@RequestBody User user) { 
+		System.out.println("login controller");
+		User principal = userService.로그인(user);
+		
+		if(principal != null) {
+			session.setAttribute("principal", principal);
+		}
+		
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+	}
 }
