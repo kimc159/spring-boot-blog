624658c7e5427d9620c7b69b3b4366086c40a9bb
leejuyeol <ljykimc159@gmail.com>
2021-07-02 10:12:18 +0900

    응답 DTO 생성

---
+++ blog/src/main/java/com/springboot/blog/dto/ResponseDto.java
@@ -0,0 +1,19 @@
+package com.springboot.blog.dto;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ResponseDto<T> {
+	
+	HttpStatus status; 
+	T data;
+	
+}
