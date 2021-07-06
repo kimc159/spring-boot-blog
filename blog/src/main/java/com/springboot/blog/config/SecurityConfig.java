935c8214d4960f7eba372186e497df7ced6ab2c2
leejuyeol <ljykimc159@gmail.com>
2021-07-06 10:01:44 +0900

    비밀번호 암호화 security 설정 추가

--- blog/src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ blog/src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.springboot.blog.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,10 +19,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		return new BCryptPasswordEncoder();
 	}
 	
+	// 시큐리티가 대신 로그인해주는데 password를 가로채기를 하는데
+	// 해당 password가 뭘로 해쉬가 되어 회원가입이 되었는지 알아야
+	// 같은 해쉬로 암호화해서 DB에 있는 해쉬랑 비교할 수 있음
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(null).passwordEncoder(encodePWD());
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-			.csrf().disable() // csrf 토큰 비활성화 (테스트시 걸어두는게 좋음)
+			.csrf().disable() // csrf 토큰 비활성화 (테스트시 걸어두는게 좋음), 시큐리티가 자동적으로 csrf 토큰이 없을 경우 접근을 막음
 			.authorizeRequests() // request가 들어오면
 				.antMatchers("/", "/auth/**", "/js/**", "/css/**", "/images/**")  
 				.permitAll()// 누구나 들어올 수 있다. 
@@ -29,7 +38,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.authenticated() // 인증이 되야됨
 			.and()
 				.formLogin()
-				.loginPage("/auth/loginForm");  
+				.loginPage("/auth/loginForm")
+				.loginProcessingUrl("/auth/loginProc") // 스프링 시큐리티가 해당 주소로 요청하는 로그인을 가로채서 대신 로그인 해준다.
+				.defaultSuccessUrl("/"); // 로그인이 정상일 때 이 주소로 옴
+				//.failureUrl("/fail") 실패했을때
 		
 	}
 }
